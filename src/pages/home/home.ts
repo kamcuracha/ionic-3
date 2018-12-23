78ba30a93d434920ad74645cb968ee4d76bd90fe
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-23 23:10:25 +0800

    Show posts; Add skeleton markup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,6 +11,10 @@ export class HomePage {
 
   postsObsrv: Observable<any>;
   posts: Post[] = [];
+  isLoading: boolean = true;
+  isGettingPosts: boolean = false;
+  Arr = Array;
+  skeletons: number = 10;
 
   constructor(public navCtrl: NavController,
     public http: HttpClient) {
@@ -18,12 +22,19 @@ export class HomePage {
   }
 
   getPosts() {
+    this.isGettingPosts = true;
+
     let url = "https://jsonplaceholder.typicode.com/posts/";
     this.postsObsrv = this.http.get<Post>(url);
     this.postsObsrv.subscribe( res => {
       res.forEach( resItem => {
         this.posts.push(resItem);
       });
+
+      this.isLoading = false;
+    },
+    err => {
+      alert('Oops:' + err.message);
     });
   }
 
