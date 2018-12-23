d2277ed74713a196baad5a561a7adfcca3534525
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-23 21:05:52 +0800

    Get posts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  postsObsrv: Observable<any>;
+  posts: Post[] = [];
+
+  constructor(public navCtrl: NavController,
+    public http: HttpClient) {
 
   }
 
+  getPosts() {
+    let url = "https://jsonplaceholder.typicode.com/posts/";
+    this.postsObsrv = this.http.get<Post>(url);
+    this.postsObsrv.subscribe( res => {
+      res.forEach( resItem => {
+        this.posts.push(resItem);
+      });
+    });
+  }
+
+}
+
+export class Post {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
 }
