2110695505fb73d9d427596bf40dd6fe7bc74d1e
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-25 16:53:38 +0800

    Http `Post` request

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  postsObsrv: Observable<any>;
+  postDataLoad: any;
+  postDataReturn: any;
 
+  constructor(public navCtrl: NavController,
+    public http: HttpClient) {
+
+  }
+
+  postData() {
+    let url = "https://reqres.in/api/users";
+    this.postDataLoad = {
+      "name": "Customer004",
+      "email": "customer004@email.com",
+    }
+    this.postsObsrv = this.http.post(url, this.postDataLoad);
+    this.postsObsrv.subscribe( res => {
+      this.postDataReturn = res;
+    });
   }
 
 }
