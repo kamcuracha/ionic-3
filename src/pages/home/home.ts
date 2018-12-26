fef329ff0767a197088f06b39e3748b01b1f7705
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-26 20:34:59 +0800

    Add refresher and infinite scroll

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,6 +11,7 @@ export class HomePage {
 
   data: Observable<any>;
   items: any;
+  url: string = "https://jsonplaceholder.typicode.com/posts/";
   public contentLength: number = 180;
 
   constructor(public navCtrl: NavController,
@@ -19,9 +20,28 @@ export class HomePage {
   }
 
   getData() {
-    this.data = this.http.get("https://jsonplaceholder.typicode.com/posts/");
+    this.data = this.http.get(this.url);
     this.data.subscribe( res => {
       this.items = res;
+    });
+  }
+
+  doRefresh(e: any) {
+    console.log('Begin async refresh operation', e);
+
+    this.data.subscribe( res => {
+      this.items = res;
+      e.complete();
+    });
+  }
+
+  doInfinite(e: any) {
+    console.log('Begin async infinite-scroll operation');
+
+    this.data = this.http.get(this.url);
+    this.data.subscribe( res => {
+      this.items = this.items.concat(res);
+      e.complete();
     });
   }
 
