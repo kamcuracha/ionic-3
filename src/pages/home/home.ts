b72d0c688790860a10724b8dfea6ff3ff351f0a8
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-26 18:18:07 +0800

    Responsive grid cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data: Observable<any>;
+  items: any;
 
+  constructor(public navCtrl: NavController,
+    public http: HttpClient) {
+      this.getData();
+  }
+
+  getData() {
+    this.data = this.http.get("https://jsonplaceholder.typicode.com/posts/");
+    this.data.subscribe( res => {
+      this.items = res;
+    });
   }
 
 }
