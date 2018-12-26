943df17d2b83946aacc1a0fa0a63aec4a152b602
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-26 14:30:52 +0800

    Add action sheet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  text: string;
 
+  constructor(public navCtrl: NavController,
+    public actionSheetCtrl: ActionSheetController) {
+
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            this.text = 'Destructive clicked';
+          }
+        },
+        {
+          text: 'Archive',
+          handler: () => {
+            this.text = 'Archive clicked';
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.text = 'Cancel clicked';
+          }
+        }
+      ]
+    });
+ 
+    actionSheet.present();
   }
 
 }
