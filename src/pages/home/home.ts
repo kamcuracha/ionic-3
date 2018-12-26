9a63ce091bfaab805393f4b9f041911c9a9c2a53
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-26 17:52:30 +0800

    Add slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild(Slides) slides: Slides;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    console.log('Current index is', currentIndex);
   }
 
 }
