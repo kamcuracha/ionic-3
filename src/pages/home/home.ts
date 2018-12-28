3f7415a0a0492681cc0f8754ed5484d404d59b41
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-28 15:04:30 +0800

    Add localstorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,27 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   splash: boolean = true;
+  inputText: string;
+  key: string = 'inputValue';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private storage: Storage) {
 
   }
 
   ionViewDidLoad() {
     setTimeout(() => this.splash = false, 4000);
+  }
+
+  saveData(e: any) {
+    this.storage.set(this.key, this.inputText);
+    alert('Input saved!');
+  }
+
+  loadData(e: any) {
+    this.storage.get(this.key).then((val) => {
+      alert('The input is: ' + val);
+    });
   }
 
 }
