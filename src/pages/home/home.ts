079c0e9ef7c24a1caba2448b62141030b0bd2593
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-29 21:07:04 +0800

    Add picture from gallery

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -38,4 +38,22 @@ export class HomePage {
 
   }
 
+  openGallery() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
 }
