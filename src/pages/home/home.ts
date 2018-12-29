3d8556cb868116498a5ce2d3bd9a30d5d2669fd4
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-29 20:53:57 +0800

    Take pictures

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,33 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   splash: boolean = true;
+  base64Image: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private camera: Camera) {
 
   }
 
   ionViewDidLoad() {
     setTimeout(() => this.splash = false, 4000);
+  }
+
+  openCamera() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+
   }
 
 }
