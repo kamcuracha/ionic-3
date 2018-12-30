8c3d74d5e1556a870965fbf64248e5237ec0950a
Paul James Curacha <kamcuracha@yahoo.com>
2018-12-30 22:49:24 +0800

    Add geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,25 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   splash: boolean = true;
+  data: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private geolocation: Geolocation) {
 
   }
 
   ionViewDidLoad() {
     setTimeout(() => this.splash = false, 4000);
+  }
+
+  getLocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      this.data = 'Lat: ' + resp.coords.latitude + ', Lng: ' + resp.coords.longitude;
+     }).catch((error) => {
+      this.data = 'Error getting location: ' + error.message;
+     });
   }
 
 }
