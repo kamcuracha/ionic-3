c15ac9dab46f38fd2b2193dd7f82c70261673956
Paul James Curacha <kamcuracha@yahoo.com>
2019-01-02 21:46:48 +0800

    Add google login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,24 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   splash: boolean = true;
+  user: any = {};
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private googlePlus: GooglePlus) {
 
   }
 
   ionViewDidLoad() {
     setTimeout(() => this.splash = false, 4000);
+  }
+
+  loginGoogle() {
+    this.googlePlus.login({})
+      .then(res => {
+        this.user = res;
+        console.log(res);
+      })
+      .catch(err => console.error(err));
   }
 
 }
