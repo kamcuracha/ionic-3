100bcfe5c47ac55a4f68aa14dde1aa515b425689
Paul James Curacha <kamcuracha@yahoo.com>
2019-01-03 15:29:58 +0800

    Add onesignal (fcm wrapper) push notifcation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { OneSignal } from '@ionic-native/onesignal';
+import { ENV_CONFIG } from './environment';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,12 +12,26 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private oneSignal: OneSignal) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.oneSignal.startInit(ENV_CONFIG.oneSignal, ENV_CONFIG.firebaseId);
+
+      this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+      this.oneSignal.handleNotificationReceived().subscribe((res) => {
+        console.log(res);
+      });
+
+      this.oneSignal.handleNotificationOpened().subscribe((res) => {
+        console.log(res);
+      });
+
+      this.oneSignal.endInit();
     });
   }
 }
---
+++ src/app/environment.ts
@@ -0,0 +1,4 @@
+export const ENV_CONFIG = {
+  oneSignal: '45fc1de8-2dc0-4874-afd1-18f5b4b72223',
+  firebaseId: '498393460391'
+};
