05862afc7d04d9cc7d4f74cf63d045b665bbbb4c
Paul James Curacha <kamcuracha@yahoo.com>
2019-01-13 11:21:24 +0800

    Add facebook login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,28 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   splash: boolean = true;
+  user: any = {};
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private fb: Facebook) {
 
   }
 
   ionViewDidLoad() {
     setTimeout(() => this.splash = false, 4000);
+  }
+
+  loginFacebook() {
+    this.fb.login(['public_profile', 'email'])
+      .then((res: FacebookLoginResponse) => {
+        if(res.status === 'connected') {
+          this.user.img = 'http://graph.facebook.com/' + res.authResponse.userID + '/picture?type=square';
+          console.log('Logged into Facebook!', res);
+        } else {
+          alert('Login failed!');
+        }
+      })
+      .catch(e => console.log('Error logging into Facebook', e));
   }
 
 }
