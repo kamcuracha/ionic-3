37c803bae1bfabfec8d1e0d8ee76d0339d7462b7
Paul James Curacha <kamcuracha@yahoo.com>
2019-01-14 02:35:26 +0800

    Get `me` data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -10,9 +11,11 @@ export class HomePage {
 
   splash: boolean = true;
   user: any = {};
+  objectKeys = Object.keys;
 
   constructor(public navCtrl: NavController,
-    private fb: Facebook) {
+    private fb: Facebook,
+    private http: HttpClient) {
 
   }
 
@@ -25,12 +28,27 @@ export class HomePage {
       .then((res: FacebookLoginResponse) => {
         if(res.status === 'connected') {
           this.user.img = 'http://graph.facebook.com/' + res.authResponse.userID + '/picture?type=square';
-          console.log('Logged into Facebook!', res);
+          this.getData(res.authResponse.accessToken);
         } else {
           alert('Login failed!');
         }
+
+        
+        console.log('Logged into Facebook!', res);
       })
       .catch(e => console.log('Error logging into Facebook', e));
+  }
+
+  getData(token: string) {
+    let url = 'https://graph.facebook.com/me?fields=id,name,first_name,last_name,email&access_token=' + token;
+
+    this.http
+      .get(url)
+      .subscribe((res:any) => {
+        this.user.fname = res.first_name;
+        this.user.lname = res.last_name;
+        this.user.email = res.email;
+      });
   }
 
 }
